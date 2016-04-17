38d2808f71720a2f8d65d3ddf299b72f09cf0ad8
Atmajr <jratma@gmail.com>
2016-04-16 22:47:07 -0400

    adding MazeDirection

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -27,10 +27,10 @@ public class Maze : MonoBehaviour {
 		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 		cells = new MazeCell[size.x, size.z];
 		IntVector2 coordinates = RandomCoordinates;
-		while (ContainsCoordinates(coordinates)) {
+		while ((ContainsCoordinates(coordinates)) && (GetCell(coordinates) == null)) {
 			yield return delay;
 			CreateCell(coordinates);
-			coordinates.z += 1;
+			coordinates += MazeDirections.RandomValue.ToIntVector2();
 		}
 	}
 
@@ -41,6 +41,10 @@ public class Maze : MonoBehaviour {
 		newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+	}
+
+	public MazeCell GetCell (IntVector2 coordinates) {
+		return cells[coordinates.x, coordinates.z];
 	}
 
 }
