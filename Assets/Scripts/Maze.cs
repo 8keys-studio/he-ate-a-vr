69736addb63acc0b667a7aa3f06c2be37394573e
Atmajr <jratma@gmail.com>
2016-04-16 23:08:31 -0400

    Create scripts for MazeWall, MazePassage and MazeCellEdge

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Maze : MonoBehaviour {
 
@@ -26,21 +27,46 @@ public class Maze : MonoBehaviour {
 	public IEnumerator Generate () {
 		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 		cells = new MazeCell[size.x, size.z];
-		IntVector2 coordinates = RandomCoordinates;
-		while ((ContainsCoordinates(coordinates)) && (GetCell(coordinates) == null)) {
+		List<MazeCell> activeCells = new List<MazeCell>();
+		DoFirstGenerationStep(activeCells);
+		//IntVector2 coordinates = RandomCoordinates;
+		//while ((ContainsCoordinates(coordinates)) && (GetCell(coordinates) == null)) {
+		//	yield return delay;
+		//	CreateCell(coordinates);
+		//	coordinates += MazeDirections.RandomValue.ToIntVector2();
+	//}
+		while (activeCells.Count > 0) {
 			yield return delay;
-			CreateCell(coordinates);
-			coordinates += MazeDirections.RandomValue.ToIntVector2();
+			DoNextGenerationStep(activeCells);
+		}
+
+	}
+
+	private void DoFirstGenerationStep (List<MazeCell> activeCells) {
+		activeCells.Add(CreateCell(RandomCoordinates));
+	}
+
+	private void DoNextGenerationStep (List<MazeCell> activeCells) {
+		int currentIndex = activeCells.Count - 1;
+		MazeCell currentCell = activeCells[currentIndex];
+		MazeDirection direction = MazeDirections.RandomValue;
+		IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
+		if (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
+			activeCells.Add(CreateCell(coordinates));
+		}
+		else {
+			activeCells.RemoveAt(currentIndex);
 		}
 	}
 
-	private void CreateCell (IntVector2 coordinates) {
+	private MazeCell CreateCell (IntVector2 coordinates) {
 		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
 		cells[coordinates.x, coordinates.z] = newCell;
 		newCell.coordinates = coordinates;
 		newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+		return newCell;
 	}
 
 	public MazeCell GetCell (IntVector2 coordinates) {
--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -3,4 +3,14 @@ using System.Collections;
 
 public class MazeCell : MonoBehaviour {
 	public IntVector2 coordinates;
+
+	private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+
+	public MazeCellEdge GetEdge (MazeDirection direction) {
+		return edges[(int)direction];
+	}
+
+	public void SetEdge (MazeDirection direction, MazeCellEdge edge) {
+		edges[(int)direction] = edge;
+	}
 }
